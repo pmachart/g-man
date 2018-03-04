704d6543d18d234c1104089f9e8c6ae06d14e347
PMA <pierre-marie.achart@gmail.com>
2018-03-04 23:31:26 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# g-man
-a simple bash script to accelerate your daily git usage
+# G-Man
+<img src="images/suitcase.jpg" height="130" align="right">
+A simple bash script to accelerate your daily git usage.
+
+----
+## Description
+The script is built on top of `git status -s` to generate a numbered file list.
+
+That list can then be used to run various actions on files or group of files by typing only these numbers and shorthand commands such as `g 2 a` to to a `git add` on the second file in the list. No more path copy-pasting.
+
+----
+## Installation
+Using "g" as a shorthand for `git` is the obvious thing to do.
+Just copy the file and use a bash alias to run it, like so :
+```shell
+alias g='~/gman.sh'
+```
+
+----
+## Usage
+
+| CLI | Details |
+| --- | --- |
+| <img src="images/usage01.png"> | Basic usage : specify a file's number, followed by the desired action. |
+| <img src="images/usage02.png" width="232"> | Number range : in case you are too lazy. One could also have typed `g 3 4 5 6 a` for the same outcome. |
+| <img src="images/usage03.png"> | You can chain instructions, following the "file numbers - action" pattern.<br><br>In this example, files 1 2 and 3 are staged, file 6 is checked out, and we `cd` to the folder containing file 5. |
+
+
+(Prompt on these screenshots is with <a href="https://github.com/magicmonty/bash-git-prompt" target="_blank">MagicMonty's bash-git-prompt</a>)
+
+----
+## Available commands
+This is just a short list of the available actions. For more details, and to customize them or add your own, take a look at the script. Contributions are very welcome.
+
+#### Git related
+| Command and shorthand | Description |
+| --- | --- |
+| add, a | `git add` |
+| oops | adds file to the last non-pushed commit |
+| reset | `git reset` |
+| rm | `git rm` |
+| checkout, co, u | reverts changes to staged or unstaged file |
+
+#### Filesystem related
+| Command and shorthand | Description |
+| --- | --- |
+| cd | `cd` to the folder containing the file |
+| vim, v | open the file with vim |
+| nano, n | open the file with nano |
+| atom, o | open the file with atom |
+
+----
+## Contribution
+
+I have built this for my personal use. I am a front-end web developer and bash scripting is not my usual language.
+
+Many former colleagues have seen me use this small tool and asked me to share it with them, and i did. With this repository, I am taking things a step further.
+
+I have improved it and refactored it a few times and am always looking for advice and constructive criticism, so please feel free to contribute or comment about anything.
---
+++ g.sh
@@ -0,0 +1,156 @@
+#! /bin/bash
+# @TODO : mute les output intermediaires de git reset en groupe
+
+function gman() {
+
+  local GITROOT=$(git rev-parse --show-toplevel)
+  local WORKDIR=$PWD
+  local RELDIR=${WORKDIR#$GITROOT}
+
+  if [[ $# == 0 ]] ; then # no param : build list
+
+    unset gitstatus # global var
+    gitstatus=$(git status --porcelain | cut -c 4-) #porcelain always does absolute paths
+
+    if [ ! -z "$gitstatus" ] ; then
+      echo ''
+      cd $GITROOT # status -s always does relative paths, this is a small hack to avoid displaying a bunch of ../
+      git -c color.status=always status -s | nl -ba -s' '
+      cd $OLDPWD
+      echo ''
+    else
+      echo -e '\n   "Looks clear."\n'
+    fi
+
+    return # end build list : exit script
+  fi
+
+
+
+  local files=()
+  local file=''
+  local args=( "$@" )
+  local hasaction=false
+  local first=''
+  local last=''
+
+  for arg in ${args[@]} ; do # loop through all args, both files and actions
+
+    if grep -v -q '^[a-zA-Z]*$' <<< "${arg}" ; then # not an action
+
+      if [[ $arg =~ ^[0-9]+-[0-9]+$ ]] ; then # parameter is range of numbers
+        first=$(echo $arg | cut -f1 -d-)
+        last=$(echo $arg | cut -f2 -d-)
+
+        if [[ -z $first || -z $last ]] ; then
+          echo 'error : invalid range '$arg; return # user error : exit
+        fi
+
+        if [[ $first > $last ]] ; then # swap range
+          local temp=$last
+          last=$first
+          first=$temp
+        fi
+
+      else # single parameter
+
+        if grep -q '^[0-9]*$' <<< "${arg}" ; then # numeric check
+          first=$arg # @TODO single file hack
+          last=$arg
+        else
+          echo 'error : invalid parameter '$arg; return # user error : exit
+        fi
+
+      fi # end single parameter
+
+      for (( i=$first; i<${last}+1; i++ )) ; do # file lists are built
+        file=$(sed -n ${i}p <<< "$gitstatus")
+        files=($(echo ${files[@]}) "$file") # array.push in bash :)
+      done
+
+    else # actions go here
+      hasaction=true
+
+      for file in ${files[@]} ; do
+        case $arg in
+
+          # basic actions
+          'cd')
+            cd $GITROOT/$(dirname "${file}")
+            echo '' ; ls ; echo ''
+            ;;
+          'vim' | 'v')
+            vim $GITROOT/$file
+            ;;
+          'nano' | 'n')
+            nano $GITROOT/$file
+            ;;
+          'atom' | 'o')
+            atom $GITROOT/$file
+            ;;
+
+          # adding resetting and undoing
+          'add' | 'a')
+            git add $GITROOT/$file
+            ;;
+          'oops') # add to the latest unpushed commit
+            git add $GITROOT/$file
+            git commit --amend --no-edit --no-verify
+            ;;
+          'reset' | 'r')
+            git reset $GITROOT/$file
+            ;;
+          'rm')
+            git rm $GITROOT/$file
+            ;;
+          'checkout' | 'co' | 'u')
+            if [[ $(git diff-index --cached HEAD --) == *"$file" ]]; then # file is staged ?
+              git reset HEAD $GITROOT/$file # avoids unnecessary output on git checkout
+            fi
+            git checkout $GITROOT/$file
+            ;;
+
+          # diffs
+          'diff' | 'df')
+            git diff $GITROOT/$file # full diff
+            ;;& # fallthrough
+          'd') # short diff
+            git diff --unified=0 --ignore-space-at-eol --color-words='[[:alnum:]]+|[^[:space:]]' $GITROOT/$file
+            ;;&
+          'diff' | 'df' | 'dc' | 'd')
+            git diff --cached $GITROOT/$file # cached diff
+            ;;
+
+          *)
+            echo unrecognized action : \"$arg\" on file \"$file\"
+            ;;
+
+        esac
+      done
+
+      files=() # reset file lists for optional next action
+
+    fi # end actions
+
+  done # end loop through args
+
+  if [[ $hasaction == false ]] ; then # no action arg > output abs paths
+    local output=""
+    local clipboard=""
+
+    for file in ${files[@]} ; do
+      output+="$GITROOT/$file\n" # newline separated list for output
+      clipboard+="$GITROOT/$file " # space separated list for clipboard
+    done
+
+    echo -e "\n"$output
+
+    if [[ ! -z $(which xclip) ]] ; then
+      echo -n " $clipboard" | xclip
+    else
+      echo 'apt install xclip to send this output to clipboard next time'
+    fi
+  fi
+}
+
+gman $@ # call function wrapper with all arguments. allows for local variables.
